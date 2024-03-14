24ababebe94bdef47d72968f0ccd8062149c73e7
weg-coder-rohit <rohit.singh042499@gmail.com>
2024-03-14 14:09:10 +0530

    changes something is source code

--- src/main/java/com/visualpathit/account/controller/UserController.java
+++ src/main/java/com/visualpathit/account/controller/UserController.java
@@ -16,7 +16,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import io.prometheus.client.Counter;
+import io.prometheus.client.exporter.MetricsServlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,6 +31,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+
 /**{@author waheedk}*/
 @Controller
 public class UserController {
